83feb4a352dabfe6dd06ebe48f6af0b8a7bd5fe3
neldevfull <nfrugeri@gmail.com>
2016-08-30 23:48:05 +0000

    Implemented WSGI with gunicorn

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+application = get_wsgi_application()
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
