33e8ca3087e0d9bf24b90aefa6764371baa70d56
neldevfull <nfrugeri@gmail.com>
2016-09-01 13:39:49 +0000

    Implemented cache

--- steptwo/app.py
+++ steptwo/app.py
@@ -1,39 +1,51 @@
+import os
+import hashlib
 from django.conf.urls import url
 from django.http import HttpResponse, HttpResponseBadRequest
 from django import forms
 from io import BytesIO
 from PIL import Image, ImageDraw
+from django.core.cache import cache
+from django.views.decorators.http import etag
 
 
 class ImageForm(forms.Form):
-    # Responsible for validate the placeholder image
+    """Responsible for validate the placeholder image"""
     width  = forms.IntegerField(min_value=1, max_value=2000)
     height = forms.IntegerField(min_value=1, max_value=2000)
 
     def generate(self, image_format='PNG'):
-        width  = self.cleaned_data['width']
-        height = self.cleaned_data['height']
+        width   = self.cleaned_data['width']
+        height  = self.cleaned_data['height']
+        key     = '{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
 
-        image = Image.new('RGB', (width, height))
-        draw  = ImageDraw.Draw(image)
-        text  = '{} x {}'.format(width, height)
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw  = ImageDraw.Draw(image)
+            text  = '{} x {}'.format(width, height)
 
-        textwidth, textheight = draw.textsize(text)
+            textwidth, textheight = draw.textsize(text)
 
-        if textwidth < width and textheight < height:
-            texttop  = (height - textheight) // 2
-            textleft = (width - textwidth) // 2
-            draw.text((textleft, texttop), text, fill=(255, 255, 255))
+            if textwidth < width and textheight < height:
+                texttop  = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft, texttop), text, fill=(255, 255, 255))
 
-        content = BytesIO()
-        image.save(content, image_format)
-        content.seek(0)
+            content = BytesIO()
+            image.save(content, image_format)
+            content.seek(0)
+            cache.set(key, content, 60 * 60)
 
         return content
 
+def generate_etag(request, width, height):
+    content = 'Placeholder: {} x {}'.format(width, height)
+    return hashlib.sha1(content.encode('utf-8')).hexdigest()
 
+@etag(generate_etag)
 def placeholder(request, width, height):
-    # Responsible for returning width and height passed as parameter
+    """Responsible for returning width and height passed as parameter"""
     form  = ImageForm({'width': width, 'height': height})
 
     if not form.is_valid():
--- steptwo/settings.py
+++ steptwo/settings.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 SECRET_KEY = os.environ.get('SECRET_KEY')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST', '192.168.33.11').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '192.168.33.11').split(',')
 DEBUG=DEBUG
 ROOT_URLCONF='app'
 SECRET_KEY=SECRET_KEY
