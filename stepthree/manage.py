e4ce0acadbcbae33e8ff698cced49a40de32cb4c
neldevfull <nfrugeri@gmail.com>
2016-09-01 20:12:11 +0000

    Implementing stepthree

---
+++ stepthree/manage.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+application = get_wsgi_application()
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
---
+++ stepthree/settings.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from django.conf import settings
+
+BASE_DIR=os.path.dirname(__file__)
+DEBUG=os.environ.get('DEBUG', 'on') == 'on'
+SECRET_KEY=os.environ.get('SECRET_KEY')
+ALLOWED_HOSTS=os.environ.get('ALLOWED_HOSTS', '192.168.33.11').split(',')
+ROOT_URLCONF='sitebuilder.urls'
+MIDDLEWARE_CLASSES=(
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+)
+INSTALLED_APPS=(
+    'django.contrib.staticfiles',
+    'sitebuilder',
+)
+TEMPLATES=(
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': (os.path.join(BASE_DIR, 'templates'), ),
+    },
+)
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_URL='/static/'
+SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages')
