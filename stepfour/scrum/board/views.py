f2a3f68dabf4e061a2007b10a6b671d712fdf4e7
neldevfull <nfrugeri@gmail.com>
2016-09-07 00:23:41 +0000

    Implemting filter

--- stepfour/scrum/board/views.py
+++ stepfour/scrum/board/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth import get_user_model
 from rest_framework import authentication, permissions, viewsets, filters
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+from .forms import TaskFilter
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
 
 User = get_user_model()
@@ -23,12 +26,20 @@ class DefaultsMixin(object):
     paginate_by_param = 'page_size'
     max_paginate_by = 50
 
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+
 
 class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """API endpoint for list and create sprints"""
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+    search_filter = ('name', )
+    ordering_fields = ('end', 'name', )
 
 
 class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
@@ -36,6 +47,9 @@ class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_class = TaskFilter
+    search_filter = ('name', 'description', )
+    ordering_fields = ('name', 'order', 'starded', 'due', 'completed', )
 
 
 class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
@@ -45,3 +59,4 @@ class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
     lookup_url_kwarg = User.USERNAME_FIELD
     queryset = User.objects.order_by(User.USERNAME_FIELD)
     serializer_class = UserSerializer
+    search_fields = (User.USERNAME_FIELD, )
