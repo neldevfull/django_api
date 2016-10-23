7802a7d14b1157de11672a731ad1f9d380f2350d
neldevfull <nfrugeri@gmail.com>
2016-10-23 17:28:16 +0000

    Initial development blog in django

---
+++ stepfive/nblog_project/nblog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from nblog.core.views import home
+
+
+urlpatterns = [
+    url(r'^$', home),
+    url(r'^admin/', admin.site.urls),
+]
