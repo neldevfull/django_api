addedeb2a47a4aa715dda0b82378c5c3ed0ba5fa
neldevfull <nfrugeri@gmail.com>
2016-12-02 00:47:22 +0000

    Implementation stepzero, TEST FIRST!

---
+++ stepzero/fizzbuzz.py
@@ -0,0 +1,33 @@
+"""
+
+*** Jogo FizzBuzz ***
+
+Regras
+-------------------------------------------------------------------------------
+
+- Quando a posição é múltipla de 3, fale fizz;
+- Quando a posição é múltipla de 5, fale buzz;
+- Quando a posição for múltipla de 3 e de 5, fale fizzbuzz;
+- Para todas as outras existe mastercard, brincadeira, só fale o próximo número
+"""
+
+
+def robot(pos):
+    if pos in (9, 6, 3):
+        return 'fizz'
+
+    return str(pos)
+
+
+def main():
+    assert robot(1) == '1'
+    assert robot(2) == '2'
+    assert robot(4) == '4'
+
+    assert robot(3) == 'fizz'
+    assert robot(6) == 'fizz'
+    assert robot(9) == 'fizz'
+
+
+if __name__ == '__main__':
+    main()
